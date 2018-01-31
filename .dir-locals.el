99492c6d70c49b2261a27f3e5f4f3519ae881310
Basil L. Contovounesios <contovob@tcd.ie>
2018-01-31 16:50:19 +0000

    Support bug-reference-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,7 @@
 ((nil
+  (bug-reference-bug-regexp . "\\(\\(?:issue\\|pr\\) ?\\)?#\\([[:digit:]]+\\)")
+  (bug-reference-url-format
+   . "https://github.com/clarete/hackernews.el/issues/%s")
   (fill-column . 70)
   (sentence-end-double-space . t))
  (emacs-lisp-mode
