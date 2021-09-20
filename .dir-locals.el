ccfa75c0b3d67201cdf0f2324f311544ade498db
Basil L. Contovounesios <contovob@tcd.ie>
2021-09-20 01:17:26 +0100

    Remove bug-reference setup from dir-locals-file
    * .dir-locals-file.el (bug-reference-bug-regexp)
    (bug-reference-url-format): Remove in preference of Emacs 28's more
    general DWIM setup.  Users on older Emacs versions can still add the
    relevant incantations to their personal .dir-locals-2.el file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,4 @@
 ((nil
-  (bug-reference-bug-regexp . "\\(\\(?:issue\\|pr\\) ?\\)?#\\([[:digit:]]+\\)")
-  (bug-reference-url-format
-   . "https://github.com/clarete/hackernews.el/issues/%s")
   (fill-column . 70)
   (sentence-end-double-space . t))
  (emacs-lisp-mode
